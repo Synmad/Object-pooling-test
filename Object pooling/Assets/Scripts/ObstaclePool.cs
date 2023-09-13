6de94bc080f92114bed7b41bdd7151b84f48df16
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-09-13 17:37:19 -0300

    Player controls, object pool

---
+++ Object pooling/Assets/Scripts/ObstaclePool.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePool : MonoBehaviour
+{
+    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] List<GameObject> pooledObstacles;
+    [SerializeField] int poolSize;
+
+    public static ObstaclePool Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(this); }
+        else { Instance = this; }
+    }
+
+    private void Start()
+    {
+        pooledObstacles = new List<GameObject>();
+        GameObject tmp;
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            tmp = Instantiate(obstaclePrefab);
+            tmp.SetActive(false);
+            pooledObstacles.Add(tmp);
+        }
+    }
+
+    public GameObject GetPooledObstacles()
+    {
+        for (int i = 0; i < poolSize; i++) { if (!pooledObstacles[i].activeInHierarchy) { return pooledObstacles[i]; } }
+        return null;
+    }
+}
---
+++ Object pooling/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float jumpForce;
+    
+    bool isGrounded;
+
+    Rigidbody2D rb;
+
+    private void Awake() { rb = GetComponent<Rigidbody2D>(); }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { Jump(); }
+    }
+
+    void Jump()
+    {
+        if (!isGrounded) { return; }
+
+        rb.AddForce(Vector2.up * jumpForce);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) { isGrounded = true; }
+
+        if (collision.gameObject.CompareTag("Obstacle")) 
+        { 
+            //game over;
+        }
+    }
+}
